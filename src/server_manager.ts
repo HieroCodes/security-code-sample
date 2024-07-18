8897cbcf6d73274d13dc4bca86ebd7c3ff39474b
Borel <borel@districall.com>
2024-07-18 18:42:26 +0200

    SampleSecurityCode | AWS

--- src/server_manager.ts
+++ src/server_manager.ts
@@ -13,7 +13,7 @@ const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  port: process.env.DB_PORT
+  port: parseInt(process.env.DB_PORT || '3306', 10)  // Convertir en nombre
 });
 
 db.connect((err) => {
