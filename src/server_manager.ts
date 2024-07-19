fb40c685cf42a804ce511a73a0d5b107ac477a80
Borel <borel@districall.com>
2024-07-19 11:25:12 +0200

    SampleSecurityCode | AWS

--- src/server_manager.ts
+++ src/server_manager.ts
@@ -9,7 +9,7 @@ import mysql from 'mysql2';
 
 // Créer la connexion à la base de données
 const db = mysql.createConnection({
-  host: process.env.DB_HOST || 'mon_sgbdr',
+  host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
